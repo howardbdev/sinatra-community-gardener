103fb9786b2db931bde9e792e79883b16bda5aab
howardbdev <howarddevennish@gmail.com>
2019-02-05 17:34:38 -0500

    add skeleton of app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, "lame string that will soon be hexed"
+    set :views, "app/views"
+    set :public, "public"
+  end
+
+  get '/' do
+    "hello world"
+  end
 end
