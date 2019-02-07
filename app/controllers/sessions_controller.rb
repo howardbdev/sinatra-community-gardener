83e45de7a75d14a2a6ae4d69011e2b6547e41446
howardbdev <howarddevennish@gmail.com>
2019-02-06 20:07:28 -0500

    skeleton out login actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,22 +3,15 @@ class SessionsController < ApplicationController
     erb :'/sessions/signup'
   end
 
-  post '/users' do
-    # what happens here??
-    # 1. validate params
-    if params[:user][:name].empty? || params[:user][:password].empty? || params[:user][:email].empty?
-      redirect '/signup'
-    else
-      # 2. create user
-      # 3. log user in
-      # 4. REDIRECT NOT RENDER TO ???
-      user = User.create(params[:user])
-      # do we want to sign in????? YES
-      # what is logging in??
-      # it means we have a key-value pair on the session hash that
-      # holds an identifier, usually a user's id
-      session[:user_id] = user.id
-      redirect "/users/#{user.id}"
-    end
+  get '/login' do
+
+  end
+
+  post '/login' do
+
+  end
+
+  get '/logout' do # should this be a get request??????
+
   end
 end
