2ce1c7852ca2afbb9b6114774311cbcaf57a6e1d
howardbdev <howarddevennish@gmail.com>
2019-02-06 19:37:56 -0500

    added auto-login to post users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    "hello world"
+    current_user.inspect
   end
+
+  helpers do
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
+
 end
