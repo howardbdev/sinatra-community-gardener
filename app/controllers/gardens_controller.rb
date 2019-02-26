0e02c60e9022aded995722d0dc5f938ee5371069
howardbdev <howarddevennish@gmail.com>
2019-02-26 12:19:59 -0500

    add new route and scaffold post /gardens

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,3 +1,17 @@
 class GardensController < ApplicationController
 
+  # index action -- coming soon ;)
+  get '/gardens' do
+
+  end
+
+  # render the new garden form
+  get '/gardens/new' do
+    erb :'/gardens/new'
+  end
+
+  post '/gardens' do
+   # add code to create a new garden 
+  end
+
 end
