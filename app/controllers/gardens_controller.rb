a004e2f086801ce2a5639311cae2a1a915f0b050
howardbdev <howarddevennish@gmail.com>
2019-03-05 11:53:15 -0500

    add routes for gardens - new, create, edit, update, and show

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -11,7 +11,26 @@ class GardensController < ApplicationController
   end
 
   post '/gardens' do
-   # add code to create a new garden 
+   # add code to create a new garden
+   @garden = Garden.create(params[:garden])
+   redirect "/gardens/#{@garden.id}"
+  end
+
+  get '/gardens/:id/edit' do
+    @garden = Garden.find(params[:id])
+    erb :'/gardens/edit'
+  end
+
+  patch '/gardens/:id' do
+    @garden = Garden.find(params[:id])
+    @garden.update(params[:garden])
+    redirect "/gardens/#{@garden.id}"
+  end
+
+  get '/gardens/:id' do
+    @garden = Garden.find(params[:id])
+    # now what?
+    erb :'/gardens/show'
   end
 
 end
