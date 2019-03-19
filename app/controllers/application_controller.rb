9aae568d7aca26c6783f8dbd4a7231d9a925014a
howardbdev <howarddevennish@gmail.com>
2019-03-19 11:54:19 -0400

    add helper methods to redirect if not logged in or not authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,20 @@ class ApplicationController < Sinatra::Base
     def logged_in?
       !!current_user
     end
+
+    # helper method to redirect if a user is not logged in
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect '/'
+      end
+    end
+
+    # helper to redirect if a user is not authorized to edit a garden
+    def redirect_if_not_authorized(garden)
+      if !logged_in? || !current_user.gardens.include?(garden)
+        redirect '/gardens'
+      end
+    end
   end
 
 end
