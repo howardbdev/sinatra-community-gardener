b3b10e530685bf4f2d22211980206c5c801ae0df
howardbdev <howarddevennish@gmail.com>
2019-04-17 14:24:42 -0400

    move helpers to a module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
+require_relative "./concerns/helpers"
 class ApplicationController < Sinatra::Base
+
+  include Helpers
 
   configure do
     enable :sessions
@@ -12,37 +15,5 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
-  helpers do
-    def current_user
-      User.find_by(id: session[:user_id])
-    end
-
-    def logged_in?
-      !!current_user
-    end
-
-    # helper method to redirect if a user is not logged in
-    def redirect_if_not_logged_in
-      if !logged_in?
-        flash[:warning] = "You must be logged in to view that page."
-        redirect '/'
-      end
-    end
-
-    # helper to redirect if a user is not authorized to edit a garden
-    def redirect_if_not_authorized(garden)
-      if !logged_in? || !current_user.gardens.include?(garden)
-        flash[:warning] = "You are not authorized to edit or delete that garden!"
-        redirect '/gardens'
-      end
-    end
-
-    def validate_email_uniqueness(email)
-      if User.find_by(email: email)
-        flash[:warning] = "That email address is already taken.  Please use a different one!"
-        redirect '/signup'
-      end
-    end
-  end
 
 end
