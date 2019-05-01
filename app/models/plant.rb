1064d6ca035207e1767de6e0690f2fc653a77e18
howardbdev <howarddevennish@gmail.com>
2019-04-30 20:33:03 -0400

    add comment about AR validations

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,8 @@
 class Plant < ActiveRecord::Base
   belongs_to :garden
   has_many :users, through: :garden
+
+  # AR validations are beyond the scope of this project!
+  # not required!
+  # validates :plant_type, presence: true
 end
